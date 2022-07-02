fa95f118ad7b09d8e38adcb246ad979ea226bd8e
Maksim Romanov <maksim.romanov@jetrockets.pro>
2022-07-03 02:47:08 +0400

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: [
-    // '@react-native-community',
+    '@react-native-community',
     '@jetrockets/base',
     '@jetrockets/react',
     '@jetrockets/typescript',
